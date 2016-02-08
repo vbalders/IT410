9976b48a6af7a77d6f6f8a431f4ad257fd067fdc
vbalders <vbalderas8@gmail.com>
2016-02-08 00:23:47 -0700

    functionality index.js

--- StaticServer/index.js
+++ StaticServer/index.js
@@ -1,2 +1,24 @@
-app.use(express.static('public'));
-app.use(express.static('files'));
+"use strict";
+
+var fs = require ('fs');
+var cmarguments = require ('command-line-args');
+var express = require ('express');
+var app = express();
+
+//Input
+var myinput = cmarguments([
+    { name:'directory', other: 'i', type: String },
+    { name: 'port', alias: 'pt', type: Number}
+
+]);
+
+var actions =  myinput.parse();
+var port = actions.port || 8080;
+var mydir = actions.directory || process.cwd();
+app.use(express.static(mydir));
+app.get('/', function(req, res) {
+    res.send('Files are served from directory: ' + myinput);
+});
+app.listen(port, function() {
+    console.log('Server in port: ' + port);
+});
